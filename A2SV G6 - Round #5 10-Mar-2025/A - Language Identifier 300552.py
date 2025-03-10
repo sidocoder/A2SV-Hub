0e9fb98291d30ff3f72a784c88a8510ca7934e8a
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-10 19:45:24 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/A - Language Identifier 300552.py
@@ -0,0 +1,11 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t = int(input().strip())
+for i in range(t):    
+    sentence = input().strip()
+    if sentence.endswith("po"):
+        print("FILIPINO")
+    elif sentence.endswith("desu") or sentence.endswith("masu"):
+        print("JAPANESE")
+    elif sentence.endswith("mnida"):
+        print("KOREAN")
