98924422692b44f1d5b5b7a02ec40db1221af05a
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-10 19:45:09 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/C - Escape-Proof Transfers 300551.py
@@ -0,0 +1,16 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = map(int, input().split())
+nums = list(map(int, input().split()))
+
+count = 0
+left = 0
+
+for right in range(n):
+    if nums[right] > t:
+        left = right + 1
+    elif right - left + 1 == c:
+        count += 1
+        left += 1
+
+print(count)
