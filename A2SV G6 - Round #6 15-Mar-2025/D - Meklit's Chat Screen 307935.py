af9f4443f52c5d0268aaf1f862853e3f79095f6b
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:07:27 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 15-Mar-2025/D - Meklit's Chat Screen 307935.py
@@ -0,0 +1,20 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+
+from collections import deque
+n, k = map(int, input().split())
+messages = list(map(int, input().split()))
+def chat(n, k, messages):
+    screen = deque()
+    _set = set()
+    for i in messages:
+        if i in _set:
+            continue
+        if len(screen) == k:
+            removed = screen.pop()  
+            _set.remove(removed)
+        screen.appendleft(i)  
+        _set.add(i)
+    print(len(screen))
+    print(*screen)
+chat(n, k, messages)
