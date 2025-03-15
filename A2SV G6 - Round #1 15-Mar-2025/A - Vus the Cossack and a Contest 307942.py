dc334fdd43de48055bcd0d94a8f9fbdfb972904f
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:09:47 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV G6 - Round #1 15-Mar-2025/A - Vus the Cossack and a Contest 307942.py
@@ -0,0 +1,8 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585107/problem/A
+
+n, m, k = map(int, input().split())
+
+if m >= n and k >= n:
+    print("Yes")
+else:
+    print("No")
