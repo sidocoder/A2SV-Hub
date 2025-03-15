550c8bac2a59c58480a4b16f1b4cc16eb4345027
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-16 01:34:07 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 15-Mar-2025/B - Anansi and Trip-Photographs 308114.py
@@ -0,0 +1,20 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+def arrange(row, target_difference, heights):
+    heights.sort()  
+    front = heights[:row]  
+    back = heights[row:]   
+
+    for i in range(row):
+        if back[i] - front[i] < target_difference:
+            return "NO"
+    return "YES"
+
+t = int(input())  
+results = []
+for _ in range(t):
+    r, x = map(int, input().split())  
+    heights = list(map(int, input().split()))  
+    results.append(arrange(r, x, heights))
+
+print("\n".join(results))
