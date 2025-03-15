ec7c12015dc76e6f9038c5c9e980977f47c661d4
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-16 01:24:52 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 15-Mar-2025/B - Special Matrix Quest 308107.py
@@ -0,0 +1,19 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+mid = n // 2
+
+sum_good = 0
+
+for i in range(n):
+    sum_good += matrix[i][i]
+    sum_good += matrix[i][n - 1 - i]
+
+for i in range(n):
+    sum_good += matrix[mid][i]
+    sum_good += matrix[i][mid]
+
+sum_good -= matrix[mid][mid] * 3
+
+print(sum_good)
