061b030b3dcc2f948aec0be4cd6324183197356f
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:10:29 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 15-Mar-2025/D - Repeating Cipher 307945.py
@@ -0,0 +1,13 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+num = int(input())
+word = input()
+result = ''
+j = 1
+i = 0
+while i<len(word):
+    result += word[i]
+    i+=j
+    j+=1
+
+print(result)
