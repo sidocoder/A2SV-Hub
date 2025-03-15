9577a2ca7ce43e65d0d8807dc67cce8fec8decde
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:14:16 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 15-Mar-2025/C - The Splitting Game 307950.py
@@ -0,0 +1,32 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+def solve():
+    t = int(input())  
+    
+    for _ in range(t):
+        n = int(input())  
+        s = input().strip()  
+        
+        
+        prefix_distinct = [0] * (n + 1)
+        seen = set()
+        for i in range(n):
+            seen.add(s[i])
+            prefix_distinct[i + 1] = len(seen)
+        
+        
+        suffix_distinct = [0] * (n + 1)
+        seen = set()
+        for i in range(n - 1, -1, -1):
+            seen.add(s[i])
+            suffix_distinct[i] = len(seen)
+        
+        
+        max_value = 0
+        for i in range(1, n):
+            max_value = max(max_value, prefix_distinct[i] + suffix_distinct[i])
+        
+        print(max_value)
+
+
+solve()
