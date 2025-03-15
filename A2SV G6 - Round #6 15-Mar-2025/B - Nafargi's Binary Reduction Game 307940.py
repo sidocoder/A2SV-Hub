d1e3b60a68f5b800399f679a5c49d8cb9b2a5471
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:08:05 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 15-Mar-2025/B - Nafargi's Binary Reduction Game 307940.py
@@ -0,0 +1,12 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+from collections import Counter
+n = int(input())
+str = input()
+
+count_one = str.count('1')
+count_zero = len(str) - count_one
+if count_one == count_zero:
+    print(0)
+else:
+    print(abs(count_one - count_zero))
