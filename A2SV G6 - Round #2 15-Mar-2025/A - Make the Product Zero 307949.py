f84df74c0d53851861e465e582cca192619a2bc6
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-15 22:14:11 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 15-Mar-2025/A - Make the Product Zero 307949.py
@@ -0,0 +1,8 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n = int(input())  
+numbers = list(map(int, input().split()))  
+
+minimum = min(map(abs, numbers))  
+
+print(minimum) 
