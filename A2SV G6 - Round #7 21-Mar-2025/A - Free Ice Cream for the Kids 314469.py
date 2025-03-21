a6f6b2392b972eff93f413f71b5451d9346427df
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-21 17:17:43 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 21-Mar-2025/A - Free Ice Cream for the Kids 314469.py
@@ -0,0 +1,18 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+t, first = map(int, input().split())  
+remain = first
+distressed = 0
+
+for _ in range(t):
+    sign, n = input().split()
+    n = int(n)
+
+    if sign == '+':
+        remain += n
+    elif sign == '-' and n <= remain:
+        remain -= n
+    else:
+        distressed += 1  
+
+print(remain, distressed)
