2e048d112d22b92f5823b4241c027f875137767f
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-03-28 18:02:38 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 28-Mar-2025/Number of smaller 320921.py
@@ -0,0 +1,13 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+n,m=[int(i) for i in input().split()]
+arr1=[int(i) for i in input().split()]
+arr2 = [int(i) for i in input().split()]
+i = 0
+ans = []
+for num in arr2:
+    while i < n and arr1[i] < num:
+        i += 1
+    ans.append(i)
+print(*ans)
+    
