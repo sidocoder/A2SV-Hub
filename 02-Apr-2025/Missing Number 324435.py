54f63cf1fbaf1e1e4dfb98aad4e9645414b341c6
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-03 01:23:35 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 02-Apr-2025/Missing Number 324435.py
@@ -0,0 +1,16 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        set1 = set()
+        set2 = set()
+        ans = 0
+        for num in nums:
+            set1.add(num)
+        for i in range(n + 1):
+            set2.add(i)
+        defference = set2 - set1
+        for value in defference:
+            ans += value
+        return ans
