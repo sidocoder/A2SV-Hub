676aee254013809dd638f4054b248cda8c8b34b6
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-06 23:55:25 +0300

    Created 'Set Mismatch' by A2SV Hub

---
+++ 06-Apr-2025/Set Mismatch 327588.py
@@ -0,0 +1,20 @@
+# Problem: Set Mismatch - https://leetcode.com/problems/set-mismatch/description/
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        seen = set()
+        duplicate = -1
+        total = 0
+
+        for num in nums:
+            total += num
+            if num in seen:
+                duplicate = num
+            seen.add(num)
+        
+        expected_sum = n * (n + 1) // 2
+        missing = expected_sum - (total - duplicate)
+        return [duplicate, missing]
+
+        
