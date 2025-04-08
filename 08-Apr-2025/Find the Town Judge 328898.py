d69633cdd2226df5e4bf942f4dfe1a2d5171632a
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-08 16:49:32 +0300

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 08-Apr-2025/Find the Town Judge 328898.py
@@ -0,0 +1,14 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = defaultdict(list)
+
+        for a, b in trust:
+            graph[a].append(b)
+
+        for i in range(1, n + 1):           
+            if len(graph[i]) == 0 and sum(i in value for value in graph.values()) == n - 1:
+                return i
+        
+        return -1
