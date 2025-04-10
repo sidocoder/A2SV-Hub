a61d1d367e1287d257eaf6e56a1973442e837fd3
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-10 17:47:00 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 10-Apr-2025/Top K Frequent Elements 330763.py
@@ -0,0 +1,19 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashh = Counter(nums)
+        sorted_items = sorted(hashh.items(), key=lambda x: x[1], reverse=True)
+
+        result = []
+        count = 0
+        for item in sorted_items:
+            result.append(item[0])
+            count += 1
+            if count == k:
+                break
+
+        return result
+
+
+        
