bdc4ecc363bf0e17ea61c3984ecb79a0030f5648
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-11 00:38:59 +0300

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 10-Apr-2025/A - Escaping Prison 331427.py
@@ -0,0 +1,25 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/601269/problem/A
+
+t = int(input())
+
+
+for _ in range(t):
+    n,h = map(int, input().split())
+    query = []
+    for _ in range(n):
+        w,l = map(int, input().split())
+        query.append(max(w,l))
+       
+    
+    if sum(query) >= h:
+        print('YES')
+    else:
+        print('NO')
+
+       
+
+    # _sum = sum(_max)
+    
+    
+
+
