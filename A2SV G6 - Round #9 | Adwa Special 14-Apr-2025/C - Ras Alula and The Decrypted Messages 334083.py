d4fd013d7656634d6e1f27b2d3e946e2abeeffa5
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-14 23:54:51 +0300

    Created 'C - Ras Alula and The Decrypted Messages' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 14-Apr-2025/C - Ras Alula and The Decrypted Messages 334083.py
@@ -0,0 +1,31 @@
+# Problem: C - Ras Alula and The Decrypted Messages - https://codeforces.com/gym/601269/problem/C
+
+from sys import stdin
+
+
+def input(): return stdin.readline().strip()
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    s = input()
+    w = input()
+
+    target = 0
+    window_sum = 0
+    for i in range(M):
+        target += ord(w[i])
+        window_sum += ord(s[i])
+    
+    found = False
+    for i in range(M, N):
+        if window_sum == target:
+            found = True
+            break
+        window_sum += ord(s[i])
+        window_sum -= ord(s[i - M])
+    
+    if window_sum == target or found:
+        print("YES")
+    else:
+        print("NO")
