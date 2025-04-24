5ac3c0a0c6a20eea6252d85225135539fdac1d4c
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-04-25 01:03:44 +0300

    Created 'A - Mahmoud and Ehab and the even-odd game' by A2SV Hub

---
+++ A2SV G6 - Round #10 24-Apr-2025/A - Mahmoud and Ehab and the even-odd game 341989.py
@@ -0,0 +1,7 @@
+# Problem: A - Mahmoud and Ehab and the even-odd game - https://codeforces.com/gym/602812/problem/A
+
+n = int(input())
+if n % 2 == 0:
+    print('Mahmoud')
+else:
+    print('Ehab')
