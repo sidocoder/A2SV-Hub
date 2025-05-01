310fcf60cf1cc076c362c391f8cf4e67f91c7d83
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-05-01 22:46:10 +0300

    Created 'Generate Parentheses' by A2SV Hub

---
+++ 01-May-2025/Generate Parentheses 346629.py
@@ -0,0 +1,15 @@
+# Problem: Generate Parentheses - https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        dp = [[] for _ in range(n + 1)]
+        dp[0] = [""]
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                for inside in dp[j]:
+                    for outside in dp[i - 1 - j]:
+                        dp[i].append(f"({inside}){outside}")
+
+        return dp[n]
