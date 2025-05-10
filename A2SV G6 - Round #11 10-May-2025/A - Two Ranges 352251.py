e66b93dbca95894fe8c0e3cf725a5ad6cbc4cd5e
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-05-10 13:53:39 +0300

    Created 'A - Two Ranges' by A2SV Hub

---
+++ A2SV G6 - Round #11 10-May-2025/A - Two Ranges 352251.py
@@ -0,0 +1,14 @@
+# Problem: A - Two Ranges - https://codeforces.com/gym/604781/problem/A
+
+q = int(input())
+for _ in range(q):
+    l1,r1,l2,r2 = (map(int, input().split()))
+    choosen = l1
+    
+    for i in range(l2,r2 +1):
+        if i != choosen:
+            choosen2 = i
+            break
+    print(choosen, choosen2)
+
+   
