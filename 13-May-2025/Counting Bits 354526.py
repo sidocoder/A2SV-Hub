e25ede66d1e03d914649b4b019c7e3f11856e692
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-05-13 19:23:02 +0300

    Created 'Counting Bits' by A2SV Hub

---
+++ 13-May-2025/Counting Bits 354526.py
@@ -0,0 +1,20 @@
+# Problem: Counting Bits - https://leetcode.com/problems/counting-bits/
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        # for i in range(n+1):
+        #     ans.append(bin(i).count('1'))
+        # return ans
+
+        for num in range(n + 1):
+            count = 0
+            for i in range(32):
+                if num & (1 << i):
+                    count += 1
+            ans.append(count)
+        return ans
+
+
+
+        
