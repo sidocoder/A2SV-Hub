92498a2d00bd02390407b711b6304a056d08dc1e
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-06-01 00:48:18 +0300

    Created 'Min Cost to Connect All Points' by A2SV Hub

---
+++ 31-May-2025/Min Cost to Connect All Points 361785.py
@@ -0,0 +1,25 @@
+# Problem: Min Cost to Connect All Points - https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        visited = [False] * n
+        min_heap = [(0, 0)]  
+        total_cost = 0
+        edges_used = 0
+
+        while edges_used < n:
+            cost, u = heapq.heappop(min_heap)
+            if visited[u]:
+                continue
+            visited[u] = True
+            total_cost += cost
+            edges_used += 1
+
+            for v in range(n):
+                if not visited[v]:
+                    dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+                    heapq.heappush(min_heap, (dist, v))
+
+        return total_cost
+            
