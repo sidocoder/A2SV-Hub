b7e9a30d769d141fdccc1628eb1198290f3ed7a2
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-06-04 00:20:43 +0300

    Created 'Satisfiability of Equality Equations' by A2SV Hub

---
+++ 03-Jun-2025/Satisfiability of Equality Equations 363353.py
@@ -0,0 +1,29 @@
+# Problem: Satisfiability of Equality Equations - https://leetcode.com/problems/satisfiability-of-equality-equations/
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+       
+        parent = [i for i in range(26)]  
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])  
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        for eq in equations:
+            if eq[1] == '=':
+                x = ord(eq[0]) - ord('a')
+                y = ord(eq[3]) - ord('a')
+                union(x, y)
+
+        for eq in equations:
+            if eq[1] == '!':
+                x = ord(eq[0]) - ord('a')
+                y = ord(eq[3]) - ord('a')
+                if find(x) == find(y):
+                    return False
+
+        return True
