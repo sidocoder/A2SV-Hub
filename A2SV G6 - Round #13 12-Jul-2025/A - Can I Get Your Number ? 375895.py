bfee7eb5fee53befbea2cddac3b8581599526181
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-07-13 00:31:24 +0300

    Created 'A - Can I Get Your Number ?' by A2SV Hub

---
+++ A2SV G6 - Round #13 12-Jul-2025/A - Can I Get Your Number ? 375895.py
@@ -0,0 +1,18 @@
+# Problem: A - Can I Get Your Number ? - https://codeforces.com/gym/607625/problem/A
+
+n = int(input())
+strings = [input().strip() for _ in range(n)]
+
+first = strings[0]
+length = len(first)
+
+prefix = 0
+for i in range(length):
+    current_char = first[i]
+    for s in strings[1:]:
+        if s[i] != current_char:
+            print(prefix)
+            exit()
+    prefix += 1
+
+print(prefix)
