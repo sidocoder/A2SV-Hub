bee9c803ddee0a590b9c64c3f12942f38fadf5a5
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-07-13 00:16:49 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 12-Jul-2025/Cirno's Perfect Bitmasks Classroom 375887.py
@@ -0,0 +1,11 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if x == 1:
+        print(3)
+    elif (x & (x - 1)) == 0:
+        print(x + 1)
+    else:
+        print(x & -x)
