6f540433238fae1b91813ab10f88ae153559a765
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-07-13 00:17:44 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 12-Jul-2025/Ehab Is an Odd Person 375888.py
@@ -0,0 +1,15 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+def largestNumber(nums,n):
+    odd=[i for i in nums if i%2==1]
+    even=[i for i in nums if i%2==0]
+    if n==len(odd) or n==len(even):
+        return nums
+    # s = [str(i) for i in nums]
+    nums.sort()
+    
+        
+    return nums
+n=int(input())
+nums=[int(i) for i in input().split()]
+print(*largestNumber(nums,n))
