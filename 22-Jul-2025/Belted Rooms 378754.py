4f45505804798bbe3c454481d326b30c91b2ccd9
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-07-22 23:33:09 +0300

    Created 'Belted Rooms' by A2SV Hub

---
+++ 22-Jul-2025/Belted Rooms 378754.py
@@ -0,0 +1,15 @@
+# Problem: Belted Rooms - https://codeforces.com/problemset/problem/1428/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    
+    if '>' not in s or '<' not in s:
+        print(n)
+    else:
+        count = 0
+        for i in range(n):
+            if s[i] == '-' or s[(i - 1 + n) % n] == '-':
+                count += 1
+        print(count)
