d235411523febc4f7c125c35669c631cae2a9916
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-11-22 22:12:01 +0300

    Created 'Longest Increasing Subsequence' by A2SV Hub

---
+++ 22-Nov-2025/Longest Increasing Subsequence 409589.py
@@ -0,0 +1,18 @@
+# Problem: Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
+
+from bisect import bisect_left
+
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        if not nums: 
+            return 0
+        
+        arr = [nums.pop(0)]  
+        
+        for n in nums:
+            if n > arr[-1]:  
+                arr.append(n)
+            else:  
+                arr[bisect_left(arr, n)] = n
+
+        return len(arr)  
