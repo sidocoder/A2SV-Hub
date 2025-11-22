fd376a24ea029d76e94af497cebbf95a3258f5ca
Sifhoran Regassa <sifhoran2024@gmail.com>
2025-11-23 00:38:33 +0300

    Created 'Target Sum' by A2SV Hub

---
+++ 22-Nov-2025/Target Sum 409600.py
@@ -0,0 +1,19 @@
+# Problem: Target Sum - https://leetcode.com/problems/target-sum/
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        from functools import lru_cache
+    
+        @lru_cache(None)
+        def dp(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            return dp(i+1, total + nums[i]) + dp(i+1, total - nums[i])
+        
+        return dp(0, 0)
+
+
+
+
+
+
